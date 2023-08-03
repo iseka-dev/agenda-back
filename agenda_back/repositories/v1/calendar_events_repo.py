9d0fd283ef7b4af16de5075c239330f4b93d8719
Horacio <hchiarella@spins.com>
2023-08-02 22:00:57 -0300

    get calendar events repo

--- agenda_back/repositories/v1/calendar_events_repo.py
+++ agenda_back/repositories/v1/calendar_events_repo.py
@@ -1,7 +1,21 @@
 """Repository to query Calendar Events in postgres db."""
 
+from fastapi import Query
+from sqlmodel import Depends, Session, select
+
+from agenda_back.db.database import get_session
 from agenda_back.db.models import CalendarEvent
 
 
-def get_calendar_events() -> list[CalendarEvent]:
+def get_calendar_events(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, lte=100),
+) -> list[CalendarEvent]:
     """Get list of calendar events from database."""
+    CalendarEvent.from_orm()
+    calendar_events = session.excec(
+        select(CalendarEvent).offset(offset).limit(limit)
+    ).all()
+    return calendar_events
